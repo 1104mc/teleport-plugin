c6be11f6ed48153ce30e75513211a3f13ca3d71d
ProgrammerPeter2 <hp.raszpi@gmail.com>
2023-07-28 16:05:00 +0200

    #5 teleport kérelmek

--- src/main/java/me/peterhorvath/teleport/commands/Tp.java
+++ src/main/java/me/peterhorvath/teleport/commands/Tp.java
@@ -2,10 +2,16 @@ package me.peterhorvath.teleport.commands;
 
 import me.peterhorvath.teleport.gui.MainMenu;
 import me.peterhorvath.teleport.gui.TeleportToPlayerMenu;
+import me.peterhorvath.teleport.utils.TeleportUtil;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 
 public class Tp implements CommandExecutor {
@@ -14,8 +20,17 @@ public class Tp implements CommandExecutor {
         if(!(sender instanceof Player)) return false;
         Player player = ((Player) sender).getPlayer();
         assert player != null;
-        if(args.length == 0){
+        if(args.length == 0) {
             TeleportToPlayerMenu.openTeleportToPlayerMenu(player);
+        }else if(args.length == 1){
+            List<? extends Player> players = Bukkit.getOnlinePlayers().stream()
+                    .filter(player1 -> player1.getName().equals(args[0]))
+                    .toList();
+            if(players.isEmpty()){
+                player.sendMessage(Component.text("Player not found!", NamedTextColor.DARK_RED));
+                return false;
+            }
+            TeleportUtil.askForTeleport(player, players.get(0));
         }else if(args.length == 3){
             //TODO: go to position
         }
