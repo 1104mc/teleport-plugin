ea9d0aec7eb97dedbc35db1e64537b5606fc13a6
ProgrammerPeter2 <hp.raszpi@gmail.com>
2023-08-11 14:08:20 +0200

    #10 fixed bedrock detection

--- src/main/java/me/peterhorvath/teleport/utils/MessageUtil.java
+++ src/main/java/me/peterhorvath/teleport/utils/MessageUtil.java
@@ -5,9 +5,19 @@ import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.entity.Player;
 import org.geysermc.geyser.api.GeyserApi;
 
+import java.util.Objects;
+
 public class MessageUtil {
+    private static boolean isBedrockPlayer(Player player){
+        try {
+            return GeyserApi.api().isBedrockPlayer(player.getUniqueId());
+        }catch (RuntimeException e){
+            return Objects.requireNonNull(player.getAddress()).getAddress().toString().startsWith("127.0.0.1");
+        }
+    }
+
     public static Component getClientMessage(Player player, Component java, String bedrock, TextColor bedrockColor){
-        if(GeyserApi.api().isBedrockPlayer(player.getUniqueId())) return Component.text(bedrock, bedrockColor);
+        if(isBedrockPlayer(player)) return Component.text(bedrock, bedrockColor);
         return java;
     }
 }
